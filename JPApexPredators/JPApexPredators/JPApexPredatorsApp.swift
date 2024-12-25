0a03744b606e9e9df0e9167e0d6904de804663e6
Muharrem Efe Çayırbahçe <hi@efxlve.com>
2024-12-25 15:37:00 +0300

    Project added

---
+++ JPApexPredators/JPApexPredators/JPApexPredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPApexPredatorsApp.swift
+//  JPApexPredators
+//
+//  Created by Muharrem Efe Çayırbahçe on 23.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct JPApexPredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
